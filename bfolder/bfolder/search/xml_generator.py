a1b888576efd409bd32ed525a77d4e02d16e5e2f
evilempirer <evilempirer@gmail.com>
2012-12-15 05:58:58 -0800

    work version of xml generator

--- bfolder/bfolder/search/xml_generator.py
+++ bfolder/bfolder/search/xml_generator.py
@@ -6,6 +6,13 @@ import elementflow
 from bfolder.model import Image
 
 
+def tolower(s):
+    try:
+        return s.lower()
+    except AttributeError:
+        return ''
+
+
 def xml_gen():
     all_records = Image.objects()
     with elementflow.xml(sys.stdout, "sphinx:docset",
@@ -17,15 +24,16 @@ def xml_gen():
             xml.element("sphinx:attr", attrs=dict(name="file", type="string"))
             xml.element("sphinx:attr", attrs=dict(name="raiting", type="int"))
 
-            for record in all_records:
-                with xml.container("sphinx:document", attrs=dict(id="%d" %
-                                                                 record.sid)):
-                    xml.element('title', text=record.title)
-                    xml.element('lang', text=record.lang)
-                    xml.element('file', text=record.name)
-                    xml.element('raiting', text='%d' %
-                                int(record.raiting) if record.raiting else 0)
+        for record in all_records:
+            with xml.container("sphinx:document", attrs=dict(id="%d" %
+                                                             record.sid)):
+                xml.element('title', text=tolower(record.title))
+                xml.element('lang', text=tolower(record.lang))
+                xml.element('file', text=tolower(record.name))
+                xml.element('raiting', text='%d' %
+                            int(record.raiting) if record.raiting else 0)
     return 0
 
 
-xml_gen()
+if __name__ == '__main__':
+    sys.exit(xml_gen())
